f8985b27d5182e889e61c366a6895d614a86c5e4
koltvictor <kolts.email@gmail.com>
2023-04-03 16:52:44 -0400

    basic frame

--- app/screens/DashboardScreen.js
+++ app/screens/DashboardScreen.js
@@ -1,13 +1,30 @@
 import React from "react";
 import { useNavigate } from "react-router-native";
-import { Button, SafeAreaView, Text } from "react-native";
+import {
+  Button,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 
 export default function DashboardScreen() {
   const navigate = useNavigate();
   return (
-    <SafeAreaView>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ backgroundColor: "black", flex: 1 }} />
+      <View style={{ backgroundColor: "red", flex: 1 }} />
+      <View style={{ backgroundColor: "gold", flex: 1 }} />
       <Text>DashboardScreen</Text>
       <Button title="back to login" onPress={() => navigate("/")} />
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  nav: {
+    flexDirection: "row",
+    flex: 1,
+  },
+});
