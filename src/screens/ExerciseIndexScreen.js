08a79942ca081c1c6d6d583593a2dcbe8165b28a
koltvictor <kolts.email@gmail.com>
2023-04-18 16:00:38 -0700

    styled exerciseindex

--- src/screens/ExerciseIndexScreen.js
+++ src/screens/ExerciseIndexScreen.js
@@ -4,6 +4,7 @@ import {
   FlatList,
   TouchableOpacity,
   TextInput,
+  StyleSheet,
 } from "react-native";
 import React, { useContext, useState } from "react";
 import { DataContext } from "../app/contexts/DataContext";
@@ -29,18 +30,20 @@ export default function ExerciseIndexScreen() {
   const renderExercise = ({ item }) => {
     return (
       <TouchableOpacity
+        style={styles.exerciseContainer}
         onPress={() =>
           navigation.navigate("ExerciseDetails", { exercise: item })
         }
       >
-        <Text>{item.name}</Text>
+        <Text style={styles.exerciseName}>{item.name}</Text>
       </TouchableOpacity>
     );
   };
 
   return (
-    <View>
+    <View style={styles.container}>
       <TextInput
+        style={styles.input}
         placeholder="Search exercises"
         onChangeText={(text) => setSearchQuery(text)}
         value={searchQuery}
@@ -49,7 +52,41 @@ export default function ExerciseIndexScreen() {
         data={filteredExercises}
         renderItem={renderExercise}
         keyExtractor={(item) => item.id}
+        style={styles.exerciseList}
+        contentContainerStyle={styles.exerciseListContent}
       />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    padding: 16,
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: "#CCCCCC",
+    borderRadius: 8,
+    paddingHorizontal: 16,
+    marginBottom: 16,
+  },
+  exerciseList: {
+    flex: 1,
+  },
+  exerciseListContent: {
+    paddingBottom: 16,
+  },
+  exerciseContainer: {
+    backgroundColor: "#F7F7F7",
+    borderRadius: 8,
+    marginBottom: 8,
+    padding: 16,
+  },
+  exerciseName: {
+    fontSize: 16,
+    fontWeight: "600",
+  },
+});
