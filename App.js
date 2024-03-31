3a4cf71c9500e297b9e6714d2540c3432fa1141d
koltvictor <kolts.email@gmail.com>
2024-03-31 10:23:39 -0700

    updated SDK, wrapped App in GetureHandler

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { FirebaseAppProvider } from "reactfire";
 import { firebaseConfig } from "./src/app/firebase";
 import Provider from "./src/app/providers/Provider";
@@ -12,19 +13,21 @@ import Routes from "./src/app/routes/Routes";
 
 const App = observer(() => {
   return (
-    <FirebaseAppProvider firebaseConfig={firebaseConfig}>
-      <Provider>
-        <UserProvider>
-          <DataContextProvider>
-            <RoutineProvider>
-              <NavigationContainer>
-                <Routes />
-              </NavigationContainer>
-            </RoutineProvider>
-          </DataContextProvider>
-        </UserProvider>
-      </Provider>
-    </FirebaseAppProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <FirebaseAppProvider firebaseConfig={firebaseConfig}>
+        <Provider>
+          <UserProvider>
+            <DataContextProvider>
+              <RoutineProvider>
+                <NavigationContainer>
+                  <Routes />
+                </NavigationContainer>
+              </RoutineProvider>
+            </DataContextProvider>
+          </UserProvider>
+        </Provider>
+      </FirebaseAppProvider>
+    </GestureHandlerRootView>
   );
 });
 
