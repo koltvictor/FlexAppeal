61f93acc96c6fba5bdaae68221035a4c4a295b5c
koltvictor <kolts.email@gmail.com>
2024-04-02 10:22:33 -0700

    hiding keyboard with TouchableWithoutFeedback in RoutineScreen

--- src/screens/RoutineScreen.js
+++ src/screens/RoutineScreen.js
@@ -5,8 +5,10 @@ import {
   Text,
   TextInput,
   TouchableOpacity,
+  TouchableWithoutFeedback,
   Modal,
 } from "react-native";
+import { Keyboard } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 import { Ionicons } from "@expo/vector-icons";
 import { auth, db } from "../app/firebase";
@@ -112,54 +114,58 @@ const RoutineScreen = observer(() => {
         )}
       </ScrollView>
       <Modal visible={isModalVisible} animationType="slide">
-        <View style={styles.modalContainer}>
-          <View style={styles.errorContainer}>
-            {errorMessage ? (
-              <Text style={styles.errorMessage}>{errorMessage}</Text>
-            ) : null}
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.modalContainer}>
+            <View style={styles.errorContainer}>
+              {errorMessage ? (
+                <Text style={styles.errorMessage}>{errorMessage}</Text>
+              ) : null}
+            </View>
+            <View>
+              <TextInput
+                style={styles.modalInput}
+                placeholder="Routine Name"
+                value={routineName}
+                onChangeText={(text) => setRoutineName(text)}
+                placeholderTextColor="gray"
+              />
+            </View>
+            <View style={styles.cyclesContainer}>
+              <Text style={styles.pickerTitle}>Number of Cycles:</Text>
+              <Picker
+                selectedValue={cycles}
+                onValueChange={(value) => setCycles(value)}
+                style={styles.cyclesInput}
+              >
+                <Picker.Item label="1" value={1} />
+                <Picker.Item label="2" value={2} />
+                <Picker.Item label="3" value={3} />
+                <Picker.Item label="4" value={4} />
+                <Picker.Item label="5" value={5} />
+                <Picker.Item label="6" value={6} />
+                <Picker.Item label="7" value={7} />
+                <Picker.Item label="8" value={8} />
+                <Picker.Item label="9" value={9} />
+                <Picker.Item label="10" value={10} />
+              </Picker>
+            </View>
+            <View style={styles.modalButtonsContainer}>
+              <TouchableOpacity
+                style={styles.modalButton}
+                onPress={() => onCancel()}
+              >
+                <Text style={styles.modalButtonText}>Cancel</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.modalButton}
+                onPress={handleSaveRoutine}
+                disabled={!routineName}
+              >
+                <Text style={styles.modalButtonText}>Save</Text>
+              </TouchableOpacity>
+            </View>
           </View>
-          <TextInput
-            style={styles.modalInput}
-            placeholder="Routine Name"
-            value={routineName}
-            onChangeText={(text) => setRoutineName(text)}
-            placeholderTextColor="gray"
-          />
-          <View style={styles.cyclesContainer}>
-            <Text style={styles.pickerTitle}>Number of Cycles:</Text>
-            <Picker
-              selectedValue={cycles}
-              onValueChange={(value) => setCycles(value)}
-              style={styles.cyclesInput}
-            >
-              <Picker.Item label="1" value={1} />
-              <Picker.Item label="2" value={2} />
-              <Picker.Item label="3" value={3} />
-              <Picker.Item label="4" value={4} />
-              <Picker.Item label="5" value={5} />
-              <Picker.Item label="6" value={6} />
-              <Picker.Item label="7" value={7} />
-              <Picker.Item label="8" value={8} />
-              <Picker.Item label="9" value={9} />
-              <Picker.Item label="10" value={10} />
-            </Picker>
-          </View>
-          <View style={styles.modalButtonsContainer}>
-            <TouchableOpacity
-              style={styles.modalButton}
-              onPress={() => onCancel()}
-            >
-              <Text style={styles.modalButtonText}>Cancel</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              style={styles.modalButton}
-              onPress={handleSaveRoutine}
-              disabled={!routineName}
-            >
-              <Text style={styles.modalButtonText}>Save</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+        </TouchableWithoutFeedback>
       </Modal>
 
       <View style={styles.inputContainer}>
