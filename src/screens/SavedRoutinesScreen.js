e89baee492b2f6f7d787d9adb8f33c5bd3647a85
koltvictor <kolts.email@gmail.com>
2024-04-19 10:59:11 -0700

    Revert "share by username or email functioning"
    This reverts commit 6a4a18ca3334903b72d764a5dff97aa455476aeb.

--- src/screens/SavedRoutinesScreen.js
+++ src/screens/SavedRoutinesScreen.js
@@ -19,7 +19,7 @@ function SavedRoutinesScreen({ navigation, route }) {
     useState(false);
   const [routineToDelete, setRoutineToDelete] = useState(null);
   const [shareModalVisible, setShareModalVisible] = useState(false);
-  const [shareInput, setShareInput] = useState("");
+  const [shareEmail, setShareEmail] = useState("");
   const [shareError, setShareError] = useState("");
   const [currentUser, setCurrentUser] = useState(null);
   const [userIdToUsername, setUserIdToUsername] = useState({});
@@ -114,67 +114,37 @@ function SavedRoutinesScreen({ navigation, route }) {
   const handleShare = (routine) => {
     setRoutineToDelete(routine);
     setShareModalVisible(true);
-    setShareInput("");
+    setShareEmail("");
     setShareError("");
   };
 
   const handleShareSubmit = async () => {
     try {
-      if (shareInput.includes("@")) {
-        const snapshot = await db
-          .collection("users")
-          .where("email", "==", shareInput.toLowerCase())
-          .get();
-        if (snapshot.empty) {
-          setShareError("User not found");
-        } else {
-          let sharedUserId = null;
-          snapshot.forEach((doc) => {
-            if (doc.data().email === shareInput.toLowerCase()) {
-              sharedUserId = doc.id;
-            }
-          });
-          if (!sharedUserId) {
-            setShareError("User not found");
-          } else {
-            const sharedWith = routineToDelete.sharedWith || [];
-            if (!sharedWith.includes(sharedUserId)) {
-              sharedWith.push(sharedUserId);
-              await db
-                .collection("savedroutines")
-                .doc(routineToDelete.id)
-                .update({ sharedWith, sharedBy: currentUser });
-            }
-            setShareModalVisible(false);
-          }
-        }
+      const snapshot = await db
+        .collection("users")
+        .where("email", "==", shareEmail.toLowerCase())
+        .get();
+      if (snapshot.empty) {
+        setShareError("User not found");
       } else {
-        const snapshot = await db
-          .collection("users")
-          .where("username", "==", shareInput)
-          .get();
-        if (snapshot.empty) {
+        let sharedUserId = null;
+        snapshot.forEach((doc) => {
+          if (doc.data().email === shareEmail.toLowerCase()) {
+            sharedUserId = doc.id;
+          }
+        });
+        if (!sharedUserId) {
           setShareError("User not found");
         } else {
-          let sharedUserId = null;
-          snapshot.forEach((doc) => {
-            if (doc.data().username === shareInput) {
-              sharedUserId = doc.id;
-            }
-          });
-          if (!sharedUserId) {
-            setShareError("User not found");
-          } else {
-            const sharedWith = routineToDelete.sharedWith || [];
-            if (!sharedWith.includes(sharedUserId)) {
-              sharedWith.push(sharedUserId);
-              await db
-                .collection("savedroutines")
-                .doc(routineToDelete.id)
-                .update({ sharedWith, sharedBy: currentUser });
-            }
-            setShareModalVisible(false);
+          const sharedWith = routineToDelete.sharedWith || [];
+          if (!sharedWith.includes(sharedUserId)) {
+            sharedWith.push(sharedUserId);
+            await db
+              .collection("savedroutines")
+              .doc(routineToDelete.id)
+              .update({ sharedWith, sharedBy: currentUser });
           }
+          setShareModalVisible(false);
         }
       }
     } catch (error) {
@@ -282,14 +252,14 @@ function SavedRoutinesScreen({ navigation, route }) {
                 <View style={styles.shareModalContainer}>
                   <View style={styles.modalContent}>
                     <Text style={styles.modalText}>
-                      Enter the email address or username of the user with whom
-                      you would like to share this routine:
+                      Enter the email address of the user with whom you would
+                      like to share this routine:
                     </Text>
                     <TextInput
                       style={styles.modalInput}
-                      value={shareInput}
-                      onChangeText={setShareInput}
-                      placeholder="Email or Username"
+                      value={shareEmail}
+                      onChangeText={setShareEmail}
+                      placeholder="Email"
                       keyboardType="email-address"
                       autoCapitalize="none"
                       autoCorrect={false}
