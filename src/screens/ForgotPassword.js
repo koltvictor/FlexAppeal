c8714d18ce8073289b98b16c1d5fbdf0fce22249
koltvictor <kolts.email@gmail.com>
2024-04-29 11:48:10 -0700

    password reset email functioning

--- src/screens/ForgotPassword.js
+++ src/screens/ForgotPassword.js
@@ -1,18 +1,51 @@
 import { View, Text, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import commonStyles from "../config/styles/CommonStyles";
-import { TextInputField } from "../components/InputFieldLogin";
+import { InputField } from "../components/InputFieldSignup";
 import { auth, sendPasswordResetEmail } from "../app/firebase";
+import Toast from "react-native-toast-message";
 
 export default function ForgotPassword() {
   const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const firebaseErrorMessages = {
+    "auth/user-not-found": "Email not found",
+    "auth/invalid-email": "Invalid email",
+    "auth/too-many-requests": "Too many requests, try again later",
+  };
 
   const handlePasswordReset = async (email) => {
     try {
       await sendPasswordResetEmail(auth, email);
-      console.log("Password reset email sent successfully");
+      Toast.show({
+        type: "success",
+        position: "top",
+        topOffset: 100,
+        text1: "Success",
+        text1Style: { fontSize: 18, color: "green", textAlign: "center" },
+        text2: "Password reset email sent",
+        text2Style: { fontSize: 14, textAlign: "center" },
+        visibilityTime: 4000,
+        autoHide: true,
+        swipeable: true,
+      });
     } catch (error) {
-      console.log("Error sending password reset email", error, error.code);
+      console.log(error.code, error.message);
+      const customMessage = firebaseErrorMessages[error.code];
+
+      Toast.show({
+        type: "error",
+        position: "top",
+        topOffset: 100,
+        text1: "Hoppla!",
+        text1Style: { fontSize: 18, color: "red", textAlign: "center" },
+        text2: customMessage,
+        text2Style: { fontSize: 10, textAlign: "center" },
+        visibilityTime: 4000,
+        autoHide: true,
+        swipeable: true,
+      });
     }
   };
 
@@ -20,7 +53,7 @@ export default function ForgotPassword() {
     <View style={commonStyles.container}>
       <View style={commonStyles.centerCenter}>
         <Text style={commonStyles.text}>Forgot Password?</Text>
-        <TextInputField
+        <InputField
           iconName="mail"
           placeholder="Email Address"
           value={email}
@@ -29,7 +62,7 @@ export default function ForgotPassword() {
           autoComplete={true}
         />
         <TouchableOpacity
-          onPress={handlePasswordReset}
+          onPress={() => handlePasswordReset(email)}
           style={commonStyles.primaryButton}
         >
           <Text style={commonStyles.buttonText}>Reset Password</Text>
